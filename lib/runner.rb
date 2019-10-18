a81dfa2ba995cad965be0f22cbc1e69ecf16a8a4
Carleigh Crockett <capleugh@gmail.com>
2019-10-18 10:20:43 -0600

    Create objects from CSV

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,11 @@
 require 'CSV'
+require 'animal_lover.rb'
 
-animal = CSV.read("data/animal_lovers.csv")
+holder_array = []
+
+CSV.foreach("data/animal_lovers.csv") do |row|
+  new_hash = AnimalLover.new(row[0], row[1], row[2], row[3])
+  holder_array << new_hash
+end
+
+p holder_array
