80de05035ada10f4c0cf4b96e8639b0587394266
Lain McGratH <lain.mcgrath@gusto.com>
2019-10-18 10:23:16 -0600

    Add header to hash

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,6 +1,9 @@
 require 'CSV'
 require './lib/animal_lover.rb'
 
-CSV.foreach("./data/animal_lovers.csv") do |row|
-  require "pry"; binding.pry
+
+
+CSV.foreach("./data/animal_lovers.csv", return_headers: false, headers: :first_row,) do |row|
+  # initial = AnimalLover.new(row[0], row[1], row[2], row[3])
+  p row
 end
