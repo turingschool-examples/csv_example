ee5f195fbe427ca0114701c5606c7c2f9cb0ce68
Ana Perez Santos <a.jperez@yahoo.com>
2019-10-18 10:27:11 -0600

    Initial commit

---
+++ lib/runner.rb
@@ -0,0 +1,10 @@
+require 'csv'
+require './lib/animal_lover'
+
+holder_array = []
+
+CSV.foreach("./data/animal_lovers.csv") do |row|
+  new_hash = AnimalLover.new(row[0], row[1], row[2], row[3])
+  holder_array << new_hash
+end
+  p holder_array
