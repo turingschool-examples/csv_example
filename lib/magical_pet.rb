ddc0f7ab16c048ff0838a7d995c17f4f8675fefe
Travis Borgsmiller <travisborgsmiller@gmail.com>
2019-10-19 22:15:59 -0600

    Add magical pet

--- lib/magical_pet.rb
+++ lib/magical_pet.rb
@@ -1,8 +1,15 @@
 class MagicalPet
   attr_reader :id, :name, :pet_owner_id
+
   def initialize(id,name,pet_owner_id)
     @id = id
     @name = name
     @pet_owner_id = pet_owner_id
   end
+
+
+  def name_create
+  @name
+  end
+  
 end
--- lib/runner.rb
+++ lib/runner.rb
@@ -1,5 +1,22 @@
 require 'CSV'
+require './lib/animal_lover'
+require './lib/magical_pet'
 # headers: true & header_converter: :symbol are optional arguments
 CSV.foreach('./data/animal_lovers.csv', headers: true, header_converters: :symbol) do |row|
-  require 'pry'; binding.pry
+  id = row[:id].to_i
+  first_name = row[:first_name]
+  last_name = row[:last_name]
+  age = row[:age].to_i
+  animal_lover = AnimalLover.new(id,first_name,last_name,age)
+
+  puts "#{animal_lover.full_name} has been created!"
+end
+
+CSV.foreach('./data/magical_pets.csv', headers: true, header_converters: :symbol) do |row|
+  id = row[:id].to_i
+  name = row[:name]
+  pet_owner_id = row[:pet_owner_id].to_i
+  magical_pets = MagicalPet.new(id,name,pet_owner_id)
+
+  puts "#{magical_pets.name_create} has been created!"
 end
